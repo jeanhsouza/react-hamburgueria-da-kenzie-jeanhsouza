49966358147c1a7e33874ce8bac5967b05506375
Jean Souza <jeanhsouza@gmail.com>
2022-11-23 20:40:19 -0300

    feat/toast ok

--- kenzie-burger/src/components/Product/index.jsx
+++ kenzie-burger/src/components/Product/index.jsx
@@ -1,26 +1,39 @@
 import { Button } from "../Button";
 import { StyledLi } from "./styles";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-export function Product( {product, cart,setCart} ) {
+export function Product({ product, cart, setCart }) {
+	function addCart() {
+		const cartContains = cart.find((elem) => {
+			return elem === product;
+		});
 
-	function addCart(){
-		const cartContains = cart.find(elem =>{
-			return elem===product;
-		})
-
-		cartContains!==product && setCart((oldCart) => [...oldCart, product])
-		
+		cartContains !== product
+			? setCart((oldCart) => [...oldCart, product]) ||
+			  toast.success("Produto adicionado ao carrinho!", {
+					position: toast.POSITION.TOP_LEFT,
+			  })
+			: toast.error("Esse produto já foi adicionado ao carrinho!", {
+					position: toast.POSITION.TOP_LEFT,
+			  });
 	}
 
 	return (
 		<StyledLi>
+			<ToastContainer />
 			<div className="imgBox">
 				<img src={product.img} alt="" />
 			</div>
 			<div className="textProduct">
 				<h3>{product.name}</h3>
 				<p>{product.category}</p>
-				<span>{product.price.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}</span>
+				<span>
+					{product.price.toLocaleString("pt-br", {
+						style: "currency",
+						currency: "BRL",
+					})}
+				</span>
 				<Button click={addCart}>Adicionar</Button>
 			</div>
 		</StyledLi>
--- kenzie-burger/src/components/TotalCart/index.jsx
+++ kenzie-burger/src/components/TotalCart/index.jsx
@@ -1,18 +1,33 @@
 import { Button } from "../Button";
 import { StyledTotalCart } from "./styles";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-export function TotalCart({cart, setCart}) {
+export function TotalCart({ cart, setCart }) {
+	const totalValue = cart.reduce((acc, actualValue) => {
+		return acc + Number(actualValue.price);
+	}, 0);
 
-    const totalValue = cart.reduce((acc,actualValue)=>{
-        return acc + Number(actualValue.price)
-    },0)   
+    function removeAll(){
+        toast.success("Carrinho limpo com sucesso!", {
+            position: toast.POSITION.TOP_CENTER
+          });
+        setCart([]);
+    }
 
-	return <StyledTotalCart>
-        <div className="TextValue">
-            <h3>Total</h3>
-            <span>{totalValue.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})
-}</span>
-        </div>
-        <Button click={() => setCart([])}>Remover todos</Button>
-    </StyledTotalCart>;
+	return (
+		<StyledTotalCart>
+			<ToastContainer />
+			<div className="TextValue">
+				<h3>Total</h3>
+				<span>
+					{totalValue.toLocaleString("pt-br", {
+						style: "currency",
+						currency: "BRL",
+					})}
+				</span>
+			</div>
+			<Button click={removeAll}>Remover todos</Button>
+		</StyledTotalCart>
+	);
 }
